85cc82db7360b9793ef1e404df8e38acc14a2ee0
dhavalsinhzala <dhaval.zala@live.com>
2015-08-31 15:48:18 +0530

    add tab functionality in admin

---
+++ client/templates/admin/home.js
@@ -0,0 +1,18 @@
+Template.adminHome.helpers({
+    tabs: [{
+      header: "Stock",
+      url: "#stockTab"
+    },{
+      header: "Verify Asset",
+      url: "#verifyAssetTab"
+    },{
+      header: "Verify Asset Allocation",
+      url: "#verifyAssetAllocationTab"
+    },{
+      header: "Asset in use",
+      url: "#assetUsageTab"
+    },{
+      header: "Decomission Asset",
+      url: "#decomissionAssetTab"
+    }]
+});
