b25a91a2ea4222e53a64ca1b3ee7347c8712da42
dhavalsinhzala <dhaval.zala@live.com>
2015-09-01 17:50:43 +0530

    verify asset in admin

---
+++ client/templates/admin/stock.js
@@ -0,0 +1,34 @@
+Template.stock.helpers({
+  assets: [{
+    "type": "Laptop",
+    "make":"Dell",
+    "model":"Lattitude 3405",
+    "serialno": "LAP001",
+    "assetdetails": {
+      "oem": "with windows" ,
+      "os": "windows 10",
+      "cpu": "2401",
+      "cputype": "i5-4210U CPU @ 1.70GHz [2 core(s) x64]" ,
+      "memory": "8192",
+      "disk": " 476937"
+    }
+  },
+  {
+    "type": "Laptop",
+    "make":"Dell",
+    "model":"Lattitude 3406",
+    "serialno": "LAP002",
+    "assetdetails": {
+      "oem": "with windows" ,
+      "os": "windows 10",
+      "cpu": "2401",
+      "cputype": "i5-4210U CPU @ 1.70GHz [2 core(s) x64]" ,
+      "memory": "8192",
+      "disk": " 476937"
+    }
+  }]
+});
+
+Template.stock.events = {
+
+};
--- client/templates/admin/verify-asset.js
+++ client/templates/admin/verify-asset.js
@@ -24,3 +24,14 @@ Template.verifyAsset.helpers({
     "description": "Dell Lattitude 3450"
   }]
 });
+
+Template.verifyAsset.events = {
+  "change #header-checkbox": function(event){
+        var headerCheckbox = event.target;
+        var checkboxes = document.getElementById("verify-asset-assets").querySelectorAll(".data-grid-row .data-grid-checkbox input[type='checkbox']");
+        var checkProperty = headerCheckbox.checked;
+        for(var index = 0; index < checkboxes.length; index++){
+          checkboxes[index].checked = checkProperty;
+        }
+  }
+};
