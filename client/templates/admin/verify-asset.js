6165f563dfcf43ae533d1bb825dc81510473118c
dhavalsinhzala <dhaval.zala@live.com>
2015-09-01 15:15:03 +0530

    add data grid layout in admin

---
+++ client/templates/admin/verify-asset.js
@@ -0,0 +1,26 @@
+Template.verifyAsset.helpers({
+  assets: [{
+    "srno": 1,
+    "ponumber":"14542",
+    "supplier":"DD ComputerLand",
+    "deliverychallan": "1001",
+    "serialno": "LAP001",
+    "description": "Dell Lattitude 3450"
+  },
+  {
+    "srno": 2,
+    "ponumber":"14542",
+    "supplier":"DD ComputerLand",
+    "deliverychallan": "1001",
+    "serialno": "LAP002",
+    "description": "Dell Lattitude 3450"
+  },
+  {
+    "srno": 3,
+    "ponumber":"14542",
+    "supplier":"DD ComputerLand",
+    "deliverychallan": "1001",
+    "serialno": "LAP003",
+    "description": "Dell Lattitude 3450"
+  }]
+});
