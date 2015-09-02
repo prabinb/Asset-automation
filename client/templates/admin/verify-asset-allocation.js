17dad6acbf1627b0b4490b0251ba9ee22879d5e2
dhavalsinhzala <dhaval.zala@live.com>
2015-09-02 11:56:49 +0530

    admin ui

---
+++ client/templates/admin/verify-asset-allocation.js
@@ -0,0 +1,40 @@
+Template.verifyAssetAllocation.helpers({
+  assets: [{
+    "type": "Laptop",
+    "make":"Dell",
+    "model":"Lattitude 3405",
+    "serialno": "LAP001",
+    "assetdetails": {
+      "oem": "with windows" ,
+      "os": "windows 10",
+      "cpu": "2401",
+      "cputype": "i5-4210U CPU @ 1.70GHz [2 core(s) x64]" ,
+      "memory": "8192",
+      "disk": " 476937"
+    },
+    "emailid": "dhavalsinh.zala@imagine.com",
+    "project": "IT",
+    "manager": "dominic"
+  },
+  {
+    "type": "Laptop",
+    "make":"Dell",
+    "model":"Lattitude 3406",
+    "serialno": "LAP002",
+    "assetdetails": {
+      "oem": "with windows" ,
+      "os": "windows 10",
+      "cpu": "2401",
+      "cputype": "i5-4210U CPU @ 1.70GHz [2 core(s) x64]" ,
+      "memory": "8192",
+      "disk": " 476937"
+    },
+    "emailid": "chandan.jha@imagine.com",
+    "project": "IT",
+    "manager": "dominic"
+  }]
+});
+
+Template.verifyAssetAllocation.events = {
+
+};
