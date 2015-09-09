1e94811d5310763d56f149ac92c129da88a36419
dhavalsinhzala <dhaval.zala@live.com>
2015-09-09 17:17:34 +0530

    add datagrid search functionality

--- client/templates/admin/asset-usage.js
+++ client/templates/admin/asset-usage.js
@@ -3,18 +3,20 @@ Template.assetUsage.helpers({
     var allocationRequests = [];
     Inventory.find({assetstate: 'Allocated'}).forEach(function(inventoryItem, index){
       var asset = AssetsDetails.findOne({_id: inventoryItem.asset_id});
-      allocationRequests.push({
-        _id: inventoryItem._id,
-        srno: index + 1,
-        type: asset.type,
-        make: asset.make,
-        model:asset.model,
-        serialno: asset.serialno,
-        assetdetails: inventoryItem.assetdetails,
-        emailid: "LDAP",
-        project: "LDAP",
-        manager: "LDAP"
-      });
+      if(asset){
+        allocationRequests.push({
+          _id: inventoryItem._id,
+          srno: index + 1,
+          type: asset.type,
+          make: asset.make,
+          model:asset.model,
+          serialno: asset.serialno,
+          assetdetails: inventoryItem.assetdetails,
+          emailid: "LDAP",
+          project: "LDAP",
+          manager: "LDAP"
+        });
+      }
     });
     return allocationRequests;
   }
--- client/templates/admin/verify-asset.js
+++ client/templates/admin/verify-asset.js
@@ -1,33 +1,60 @@
-Template.verifyAsset.helpers({
-  assets: function(){
-    var verificationRequests = [];
-    AssetsProcured.find({verified: false}).forEach(function(AssetsProcuredItem, index){
-      //Get Asset Details
-      var asset = AssetsDetails.findOne({_id: AssetsProcuredItem.asset_id});
-      verificationRequests.push({
-        _id: AssetsProcuredItem._id,
-        srno: index + 1,
-        ponumber: AssetsProcuredItem.ponumber,
-        supplier: AssetsProcuredItem.supplier,
-        deliverychallan: AssetsProcuredItem.deliverychallan,
-        serialno: asset.serialno,
-        description: asset.make + " " + asset.model
-      });
+(function(){
+  var searchCriteriaObj  = new SearchCriteria(
+    {
+      verified: false
+    },
+    {
+      ponumber: "search-ponumber",
+      supplier: "search-supplier",
+      deliverychallan: "search-deliverychallan",
+      serialno: "search-serialno"
     });
-    return verificationRequests;
-  }
-});
 
-Template.verifyAsset.events({
-  'click #btn-verify-assets': function(event, template){
-    var assetRequests = template.view.template.__helpers[" assets"]();
-    var verifyIds = [];
-    assetRequests.forEach(function(assetRequest){
-      verifyIds.push(assetRequest._id);
+    Template.verifyAsset.onCreated(function() {
+      this.searchCriteria = new ReactiveVar(searchCriteriaObj.buildSearchCriteria());
     });
-    Meteor.call("verifyAssetVerificationRequests", verifyIds);
-  },
-  'click .verify-btn' : function(event){
-    Meteor.call("verifyAssetVerificationRequests", [this._id]);
-  }
-});
+
+    Template.verifyAsset.helpers({
+      assets: function(){
+        var verificationRequests = [];
+        var searchCriteria = Template.instance().searchCriteria.get();
+        var options = {
+          limit: itemsPerPage
+        };
+        AssetsProcured.find(searchCriteria.assets_procured, options).forEach(function(AssetsProcuredItem, index){
+          //Get Asset Details
+          searchCriteria.assets["_id"] = AssetsProcuredItem.asset_id;
+          var asset = AssetsDetails.findOne(searchCriteria.assets);
+          if(asset){
+            verificationRequests.push({
+              _id: AssetsProcuredItem._id,
+              srno: index + 1,
+              ponumber: AssetsProcuredItem.ponumber,
+              supplier: AssetsProcuredItem.supplier,
+              deliverychallan: AssetsProcuredItem.deliverychallan,
+              serialno: asset.serialno,
+              description: asset.make + " " + asset.model
+            });
+          }
+        });
+        return verificationRequests;
+      }
+   });
+
+    Template.verifyAsset.events({
+      'click #btn-verify-assets': function(event, template){
+        var assetRequests = template.view.template.__helpers[" assets"]();
+        var verifyIds = [];
+        assetRequests.forEach(function(assetRequest){
+          verifyIds.push(assetRequest._id);
+        });
+        Meteor.call("verifyAssetVerificationRequests", verifyIds);
+      },
+      'click .verify-btn' : function(event){
+        Meteor.call("verifyAssetVerificationRequests", [this._id]);
+      },
+      'input .search-text': function(){
+        Template.instance().searchCriteria.set(searchCriteriaObj.buildSearchCriteria());
+      }
+    });
+  })();
