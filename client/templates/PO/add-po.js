e90704849d4314a3a3a4f13c17ff64b22b8249a7
Chandan Kumar Jha <chandan1909@gmail.com>
2015-09-10 11:41:08 +0530

    changes to include the confirmation modal

--- client/templates/PO/add-po.js
+++ client/templates/PO/add-po.js
@@ -1,11 +1,18 @@
 Template.PO.helpers({
   "type": function(){
       return ["Laptop", "Headset", "Mouse", "Keyboard","LCD"];
+  },
+  "information": function(){
+    return Template.instance().information.get();
   }
 });
 
+Template.PO.onCreated(function() {
+  this.information = new ReactiveVar([]);
+});
+
 Template.PO.events({
-  "submit #addPOForm": function(event){
+  "submit #addPOForm": function(event,template){
 
     event.preventDefault();
     var form = event.target;
@@ -21,7 +28,7 @@ Template.PO.events({
     };
 
     Meteor.call("addPO", POobject, function(error, result){
-
+      template.information.set(["Purchase order placed Successfully"]);
       if(error){
         console.log("error", error);
       }
--- client/templates/security/asset-to-employee.js
+++ client/templates/security/asset-to-employee.js
@@ -4,7 +4,7 @@ Template.assetToEmployee.onCreated(function() {
   this.serialNumberList = new ReactiveVar([]);
   this.typeList = new ReactiveVar([]);
   this.selected = new ReactiveVar(null);
-
+  this.disableAssign = new ReactiveVar(true);
   Meteor.call("getAssetTypes",function(error, result){
     template.typeList.set(result);
   });
@@ -17,24 +17,46 @@ Template.assetToEmployee.helpers({
   "serialNumberList" : function(){
     return Template.instance().serialNumberList.get();
   },
-
+  "disableAssign": function(){
+    return Template.instance().disableAssign.get();
+  }
 });
 
+function checkAssign(){
+  if($("#type-select").val() && $("#serial-number-select").val() && ($("#empid").val() && $("#empid").val()!== "")) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 Template.assetToEmployee.events({
   "change #type-select": function (event, template) {
     var type = $(event.currentTarget).val();
 
     Meteor.call("findAvailableAssetsByType", type, function(error, result){
-
       if(result){
         console.log(result);
-      template.serialNumberList.set(result);
-    }
+        template.serialNumberList.set(result);
+      }
     });
+
+    if(checkAssign()){
+      template.disableAssign.set(false);
+    }
+    else{
+      template.disableAssign.set(true);
+    }
   },
   "change #serial-number-select": function (event, template) {
     var obj = $(event.currentTarget).val();
     template.selected.set(obj);
+    if(checkAssign()){
+      template.disableAssign.set(false);
+    }
+    else{
+      template.disableAssign.set(true);
+    }
   },
   "click #assign-asset-to-emp": function(event, template){
     var obj = template.selected.get();
@@ -54,5 +76,13 @@ Template.assetToEmployee.events({
         console.log("Successfully updated the inventory with empid"+empid)
       }
     });
+  },
+  "keyup #empid": function(event, template) {
+    if(checkAssign()){
+      template.disableAssign.set(false);
+    }
+    else{
+      template.disableAssign.set(true);
+    }
   }
 });
