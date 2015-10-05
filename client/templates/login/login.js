637f4f7fd5be42c13325b732a1c02c3651c6b66f
Prabin Banka <prabin.banka@imaginea.com>
2015-10-05 18:31:08 +0530

    Accessing role based pages now requires login

--- client/templates/login/login.js
+++ client/templates/login/login.js
@@ -3,22 +3,7 @@ Template.caslogin.events({
     "click #loginButton" :function(event){
         event.preventDefault();
         Meteor.loginWithCas(function(err){
-
             console.log(err);
-            
         })
     }
 })
-Accounts.onLogin(function() {
-    var user = Meteor.user();
-    var ldapId = user.profile.name;
-    //console.log(user);
-     var role = Meteor.call('findRole', ldapId, function(err, result){
-         if (result) {
-             Router.go(result);
-         }else{
-             Router.go('home');
-         }
-    });
-
-});
--- collections/methods/login.js
+++ collections/methods/login.js
@@ -1,11 +1,13 @@
 Meteor.methods({
-    findRole:function(ldapId){
-        //var role = [];
-        var rollmapping = Roles.findOne({ldapId:ldapId});
-        if (rollmapping!=undefined){
-            console.log(rollmapping.role);
-            //role.push(rollmapping.role);
-            return rollmapping.role;
+    findRole:function(ldapId) {
+     console.log("ldapId: " + ldapId);
+        if (ldapId) {
+            var rollmapping = Roles.findOne({ldapId: ldapId});
+            if (rollmapping != undefined) {
+                console.log(rollmapping.role);
+                //role.push(rollmapping.role);
+                return rollmapping.role;
+            }
         }
     }
 })
