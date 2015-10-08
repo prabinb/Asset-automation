8ed68718eec8660540916e2058962eff010a5c01
Prabin Banka <prabin.banka@imaginea.com>
2015-10-08 09:49:52 +0530

    Added the logout funtionality in all pages, except the home page.

---
+++ client/templates/logout.js
@@ -0,0 +1,8 @@
+Template.logout.events({
+    'click #logoutLink':function(event){
+        event.preventDefault();
+        Meteor.logout(function(){
+            console.log('Logged out successfully');
+        });
+    }
+})
